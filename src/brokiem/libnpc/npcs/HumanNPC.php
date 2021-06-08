a1733b3e32721c59ef292374f441e4aa5aa36dd7
brokiem <milyhqvideo@gmail.com>
2021-06-08 18:41:26 +0800

    remove identifier from npc class

--- src/brokiem/libnpc/npcs/HumanNPC.php
+++ src/brokiem/libnpc/npcs/HumanNPC.php
@@ -5,18 +5,7 @@ declare(strict_types=1);
 namespace brokiem\libnpc\npcs;
 
 use pocketmine\entity\Human;
-use pocketmine\nbt\tag\CompoundTag;
 
 class HumanNPC extends Human {
     protected $gravity = 0.0;
-    protected string $identifier;
-
-    public function getIdentifier(): string {
-        return $this->identifier;
-    }
-
-    protected function initEntity(CompoundTag $nbt): void {
-        $this->identifier = $nbt->getString("Identifier");
-        parent::initEntity($nbt);
-    }
 }
--- src/brokiem/libnpc/npcs/MobNPC.php
+++ src/brokiem/libnpc/npcs/MobNPC.php
@@ -6,22 +6,11 @@ namespace brokiem\libnpc\npcs;
 
 use pocketmine\entity\Entity;
 use pocketmine\entity\EntitySizeInfo;
-use pocketmine\nbt\tag\CompoundTag;
 
 abstract class MobNPC extends Entity {
     protected $gravity = 0.0;
-    protected string $identifier;
 
     abstract public static function getNetworkTypeId(): string;
-
-    public function getIdentifier(): string {
-        return $this->identifier;
-    }
-
-    protected function initEntity(CompoundTag $nbt): void {
-        $this->identifier = $nbt->getString("Identifier");
-        parent::initEntity($nbt);
-    }
 
     abstract protected function getInitialSizeInfo(): EntitySizeInfo;
 }
